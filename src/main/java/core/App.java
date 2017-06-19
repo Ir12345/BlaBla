37b9d591cce2d7455a9f7dfd7efada6194dc7b2f
Ir12345 <irina.ivanushkin@gmail.com>
2017-06-19 14:01:32 -0700

    Update App.java

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -6,7 +6,7 @@ package core;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String2[] args )
     {
         System.out.println( "Hello World!" );
     }
